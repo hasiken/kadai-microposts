b289f9840df1976be0aa2d95a839ba95cf164024
kentarou.hashigata <git config --global user.email taro.kirameki@techacademy.jp>
2021-04-22 01:46:14 +0000

    for heroku

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,7 +3,7 @@ class FavoritesController < ApplicationController
     micropost= Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = '投稿をいいねしました。'
-    redirect_to root_path
+    redirect_to micropost
   end
 
   def destroy
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def likes
     @user = User.find(params[:id])
     @likes = @user.user_favorites.order(id: :desc).page(params[:page])
+    counts(@user)
   end
   
   private
